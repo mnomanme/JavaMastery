eaa4d537b6104d4c6860f14e560f8a4d1d6580fe
mnomanme <abdullah.noman083@gmail.com>
2022-12-11 00:32:22 +0600

    primitive types :rocket:

---
+++ Types/src/PrimitiveTypes/Main.java
@@ -0,0 +1,11 @@
+package PrimitiveTypes;
+
+public class Main {
+    public static void main(String[] args) {
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 12.22F;
+        char letter = 'N';
+        boolean isEligible = true;
+    }
+}
