f1763bd7d5b0c7ccb313fa97edca14d3353aa340
mnomanme <abdullah.noman083@gmail.com>
2022-12-10 20:02:47 +0600

    variable declare :rocket:

---
+++ Types/src/Variables/Main.java
@@ -0,0 +1,12 @@
+package Variables;
+
+public class Main {
+    public static void main(String[] args){
+        int age = 27;
+        age = 30;
+        int myAge = 40;
+        int herAge = myAge;
+
+        System.out.println(herAge);
+    }
+}
