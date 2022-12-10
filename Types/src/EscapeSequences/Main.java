cffa1c0e62274175c93965e646d685595743c747
mnomanme <abdullah.noman083@gmail.com>
2022-12-11 01:06:17 +0600

    learn Escape Sequences :rocket:

---
+++ Types/src/EscapeSequences/Main.java
@@ -0,0 +1,10 @@
+package EscapeSequences;
+
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Hello \"Noman\"";
+        String windows = "c: \nWindows\\...";
+
+        System.out.println(myName + "\n" + windows);
+    }
+}
