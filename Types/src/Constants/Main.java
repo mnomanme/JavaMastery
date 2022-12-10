8835f724ccdf945b9b12089ad181b1cb34ccd76a
mnomanme <abdullah.noman083@gmail.com>
2022-12-11 01:37:44 +0600

    constant variable learn :rocket:

--- Types/src/Constants/Main.java
+++ Types/src/Constants/Main.java
@@ -1,2 +1,8 @@
-package Constants;public class Main {
+package Constants;
+
+public class Main {
+    public static void main(String[] args) {
+        // constant ==> final it's not to be changed
+        final float PI = 3.14F;
+    }
 }
